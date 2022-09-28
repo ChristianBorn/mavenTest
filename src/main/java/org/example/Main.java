52594729530b7b8fb9a1c8bc8800b22aabd0f443
christianborn <chrisborn@hotmail.de>
2022-09-28 12:38:37 +0200

    Add Tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static int sum(int value1, int value2) {
+        return value1 + value2;
+    }
+
+    public static String printSum(int value1, int value2) {
+        return "Summe: " + (value1 + value2);
+    }
+
+    public static boolean check100(int value1) {
+        return value1 > 100;
+    }
 }
